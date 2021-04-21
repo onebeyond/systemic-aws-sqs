d4cbbee56587345666d75d12053e4af974649846
Adrian <adrian.rodriguez@guidesmiths.com>
2021-04-21 12:08:17 +0100

    exposing the client and adding required libraries

---
+++ index.js
@@ -0,0 +1,21 @@
+const debug = require("debug")("systemic-aws-sqs");
+const { SQSClient } = require("@aws-sdk/client-sqs");
+
+const commands = require("require-all")(__dirname + "/commands");
+
+let client = null;
+
+module.exports = () => {
+  const start = async ({ config }) => {
+    debug("Initializing SQSClient");
+    client = new SQSClient(config);
+
+    return {
+      client,
+    };
+  };
+
+  return {
+    start,
+  };
+};
