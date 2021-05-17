55f376582153f6e2ac06c3dc90fa02530103f33e
Adrian <adrian.rodriguez@guidesmiths.com>
2021-05-17 12:38:48 +0200

    test sendMessage

---
+++ test/__tests__/sendMessage.test.js
@@ -0,0 +1,43 @@
+const crypto = require("crypto");
+
+const startSQSComponent = require("../helpers/startSQSComponent");
+const createSQSQueue = require("../helpers/createSQSQueue");
+const deleteSQSQueue = require("../helpers/deleteSQSQueue");
+
+const getLocalstackConfig = require("../fixtures/getLocalstackConfig");
+
+let sqs;
+let createQueue;
+let deleteQueue;
+
+const awsAccountId = "000000000000";
+
+describe("Systemic sqs Component Tests", () => {
+  beforeAll(async () => {
+    sqs = await startSQSComponent(getLocalstackConfig());
+    createQueue = createSQSQueue(sqs);
+    deleteQueue = deleteSQSQueue(sqs);
+  });
+
+  it("send a message", async () => {
+    const messageBody = "esto es un mensaje";
+    const queueName = "testQName1";
+    await createQueue(queueName);
+    const queueUrl = await sqs.getQueueUrl({
+      queueName,
+      awsAccountId,
+    });
+    const res = await sqs.sendMessage({
+      queueUrl,
+      messageBody,
+    });
+    const md5MessageBodySent = crypto
+      .createHash("md5")
+      .update(messageBody)
+      .digest("hex");
+    expect(res.$metadata.httpStatusCode).toBe(200); //enviado
+    expect(res.MD5OfMessageBody).toBe(md5MessageBodySent); // comprobación de md5
+
+    await deleteQueue(queueName);
+  });
+});
